8c0c68fad7c462122e5d0f31629754d208838885
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-03 10:21:54 +0530

    Time: 3 ms (81.43%), Space: 46.7 MB (64.18%) - LeetHub

---
+++ find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+      int[] isTrusted = new int[N+1];
+        for(int[] person : trust){
+            isTrusted[person[0]]--;
+            isTrusted[person[1]]++;
+        }
+        for(int i = 1;i < isTrusted.length;i++){
+            if(isTrusted[i] == N-1) return i;
+        }
+        return -1;
+    }
+}
