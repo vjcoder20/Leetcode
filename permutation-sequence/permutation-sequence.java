9bb3bf17dee5e293aba4461112d8c51cad12eae6
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-06 14:34:54 +0530

    Create permutation-sequence.java

--- permutation-sequence/permutation-sequence.java
+++ permutation-sequence/permutation-sequence.java
@@ -1,3 +1,46 @@
+----------------------------------------------------//Technique-1 ---> Very High time complexity O(n!*n) Brute Force-----------------------------------------------------------
+class Solution {
+    String ans = "";
+    int res = 1;
+    public String getPermutation(int n, int k) {
+        int j = n;
+        int[] arr = new int[n];
+        for(int i=n-1;i>=0;i--){
+            arr[i] = j;
+            j--;
+        }
+         boolean[] visited = new boolean[arr.length];
+        fun(arr,new ArrayList<>(),k,visited);
+        return ans;
+    }
+    
+    public void fun(int[] arr,List<Integer> al,int k,boolean[] visited){
+        if(al.size()==arr.length){
+           if(res==k){
+               for(int i:al){
+                   ans+=i;
+               }
+           }
+            res++;
+            return;
+        }
+        
+        for(int i=0;i<arr.length;i++){
+            if(visited[i]==false){
+               visited[i] = true;
+                al.add(arr[i]);
+                fun(arr,al,k,visited);
+                al.remove(al.size()-1);
+              visited[i] = false;
+            }
+        }
+    }
+    
+   
+}
+
+
+//------------------------------------------------Optimal One O(n) Solution || Explanation Striver solution--------------------------------------------------------------------------------------------------
 class Solution {
     public String getPermutation(int n, int k) {
         List<Integer> li = new ArrayList<>();
