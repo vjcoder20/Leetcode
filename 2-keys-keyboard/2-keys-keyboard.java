26faa50dbb369893394b37a3243a2c1d7075d343
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-07 23:31:11 +0530

    Time: 0 ms (100.00%), Space: 37.8 MB (30.25%) - LeetHub

---
+++ 2-keys-keyboard/2-keys-keyboard.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minSteps(int n) {
+         int ans = 0;
+		 for(int i=2;i*i<=n;i++){
+      while(n%i==0){
+          ans+=i;
+          n = n/i;
+      }
+  }
+      if(n>1)
+     ans+=n;
+     
+		return ans;
+    }
+}
