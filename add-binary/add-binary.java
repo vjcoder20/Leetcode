ab456744179a83ea2d3130ae2844b6260603be54
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-10 09:52:31 +0530

    Time: 2 ms (77.69%), Space: 39.1 MB (54.51%) - LeetHub

--- add-binary/add-binary.java
+++ add-binary/add-binary.java
@@ -7,32 +7,18 @@ class Solution {
         int carry = 0;
         while(i>=0 || j>=0 || carry>0){
             
-            int num1 = 0;
-            int num2 = 0;
+            int sum = carry;
             if(i>=0){
-               num1 = a.charAt(i)-'0'; 
+               sum += a.charAt(i)-'0'; 
             }
             if(j>=0){
-                num2 = b.charAt(j)-'0';
+               sum += b.charAt(j)-'0';
             }
-            
-            ans = num1+num2+carry;
-            if(ans==2){
-                carry = 1;
-                sb.append(0);
-            }
-            else if(ans>2){
-                carry = 1;
-                sb.append(1);
-            }
-            else{
-                carry = 0;
-                sb.append(ans);
-            }
+             sb.append(sum % 2);
+            carry = sum / 2;
             i--;
             j--;
         }
-       sb = sb.reverse();
-    return sb.toString();
+    return sb.reverse().toString();
         }  
     }
