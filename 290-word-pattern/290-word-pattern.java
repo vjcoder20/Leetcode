b1ec44342b12c0959a9d6ae8422d2330ba73a16a
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-17 14:58:38 +0530

    Time: 1 ms (86.67%), Space: 38.5 MB (31.61%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,3 +1,4 @@
+//--Smart Method
 class Solution {
    public boolean wordPattern(String pattern, String str) {
     String[] words = str.split(" ");
@@ -10,3 +11,36 @@ class Solution {
     return true;
 }
 }
+/* -->Easy Method
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if(words.length!=pattern.length()){
+            return false;
+        }
+        HashMap<Character,String> map = new HashMap<>();
+         Map<String, Boolean> map2 = new HashMap<>();
+        
+        for(int i=0;i<pattern.length();i++){
+            char ch = pattern.charAt(i);
+            if(map.containsKey(ch)==false){
+                if(map2.containsKey(words[i])==true){
+                    return false;
+                }
+                else{
+                  map.put(ch,words[i]);
+                    map2.put(words[i],true);
+                }
+                
+            }
+               else{
+                  String mapword = map.get(ch);
+                  if(mapword.equals(words[i])==false){
+                      return false;
+                  }
+               }
+        }
+        return true;
+    }
+}
+*/
