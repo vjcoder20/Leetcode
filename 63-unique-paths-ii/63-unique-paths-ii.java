cd11dc920f946d1076d3504102fb715f60d5057a
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-21 20:37:25 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (40.78%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -3,7 +3,19 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
-        return getPath(obstacleGrid,m-1,n-1,dp);
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j] = 0;
+                }
+                else if(i==0 && j==0){
+                    dp[i][j] = 1;
+                }
+                else
+             dp[i][j] = (i>=1?dp[i-1][j]:0)+(j>=1?dp[i][j-1]:0);
+            }
+        }
+        return dp[m-1][n-1];
     }
      public int getPath(int[][] arr,int sr,int sc,int[][] dp){
           
