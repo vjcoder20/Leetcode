e700bba98e9f1d6f9843e25a079f083f4f3bf513
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-24 13:01:00 +0530

    Time: 1 ms (99.35%), Space: 39.2 MB (26.92%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length()==1){
+            return true;
+        }
+        
+       
+           if((word.charAt(0)>='A' && word.charAt(0)<='Z') && (word.charAt(1)>='A' && word.charAt(1)<='Z') ){
+                for(int i=2;i<word.length();i++){
+                if(word.charAt(i)>='a' && word.charAt(i)<='z'){
+                return false;
+            }
+        }
+           }
+           else if((word.charAt(0)>='A' && word.charAt(0)<='Z') && (word.charAt(1)>='a' && word.charAt(1)<='z')){
+                for(int i=2;i<word.length();i++){
+            if(word.charAt(i)>='A' && word.charAt(i)<='Z'){
+                return false;
+            }
+        }
+           }
+            else if(word.charAt(0)>='a'){
+             for(int i=1;i<word.length();i++){
+            if(word.charAt(i)>='A' && word.charAt(i)<='Z'){
+                return false;
+            }
+        }
+    
+            }
+        return true;
+    }
+
+}
