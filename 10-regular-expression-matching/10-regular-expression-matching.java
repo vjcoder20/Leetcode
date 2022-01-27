4aa73e6426e8600c9ea42817b7427d183560dc6a
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-27 23:20:49 +0530

    Time: 6 ms (52.90%), Space: 42.8 MB (9.60%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -1,4 +1,5 @@
 class Solution {
+    
     //Recursive
     	public boolean solution(String str, String pattern,int i,int j) {
 	    
@@ -23,7 +24,56 @@ class Solution {
 	    return false;
 }
     
+    //Tabulation
+    	public boolean solution(String str, String pattern) {
+	
+	boolean[][] dp = new boolean[pattern.length()+1][str.length()+1];
+	
+	for(int i=0;i<dp.length;i++){
+	    for(int j=0;j<dp[0].length;j++){
+	    
+	    
+	    if(i==0 && j==0)
+	    dp[i][j] = true;
+	    
+	    else if(i==0){
+	        dp[i][j] = false;
+	    }
+	    
+	    else if(j==0){
+	        if(pattern.charAt(i-1)=='*')
+	        dp[i][j] = dp[i-2][j];
+	    }
+	    
+	    else{
+	        
+	        if(pattern.charAt(i-1)=='.')
+	        dp[i][j] = dp[i-1][j-1];
+	        
+	        else if(pattern.charAt(i-1)=='*'){
+	            dp[i][j] = dp[i-2][j];
+	            if(!dp[i][j] && (pattern.charAt(i-2)==str.charAt(j-1) || pattern.charAt(i-2)=='.'))
+	            dp[i][j] = dp[i][j-1];
+	        }
+	        
+	        else{
+	            if(pattern.charAt(i-1)==str.charAt(j-1))
+	            dp[i][j] = dp[i-1][j-1];
+	        }
+	        
+	    }
+	    
+	        
+	    }
+	    
+	}
+	return dp[dp.length-1][dp[0].length-1];
+		
+		
+	}
+    
     public boolean isMatch(String str, String pattern) {
-        return solution(str,pattern,0,0);
+        // return solution(str,pattern,0,0); -->Recursive Answer
+        return solution(str,pattern);  //--->Tabulation answer
     }
 }
