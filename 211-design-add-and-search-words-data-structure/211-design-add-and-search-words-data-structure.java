6186c303b28dcfaba6bcb4e57f768b29fef7f31b
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-28 17:35:07 +0530

    Time: 105 ms (27.03%), Space: 81.1 MB (15.51%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -1,44 +1,39 @@
 class WordDictionary {
-    public class TrieNode {
-        public TrieNode[] children = new TrieNode[26];
-        public String item = "";
+    private WordDictionary[] children;
+    boolean isEndOfWord;
+    // Initialize your data structure here. 
+    public WordDictionary() {
+        children = new WordDictionary[26];
+        isEndOfWord = false;
     }
     
-    private TrieNode root = new TrieNode();
-
+    // Adds a word into the data structure. 
     public void addWord(String word) {
-        TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) {
-                node.children[c - 'a'] = new TrieNode();
-            }
-            node = node.children[c - 'a'];
+        WordDictionary curr = this;
+        for(char c: word.toCharArray()){
+            if(curr.children[c - 'a'] == null)
+                curr.children[c - 'a'] = new WordDictionary();
+            curr = curr.children[c - 'a'];
         }
-        node.item = word;
-    }
-
-    public boolean search(String word) {
-        return match(word.toCharArray(), 0, root);
+        curr.isEndOfWord = true;
     }
     
-    private boolean match(char[] chs, int k, TrieNode node) {
-        if (k == chs.length) return !node.item.equals("");   
-        if (chs[k] != '.') {
-            return node.children[chs[k] - 'a'] != null && match(chs, k + 1, node.children[chs[k] - 'a']);
-        } else {
-            for (int i = 0; i < node.children.length; i++) {
-                if (node.children[i] != null) {
-                    if (match(chs, k + 1, node.children[i])) {
-                        return true;
-                    }
-                }
+    // Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. 
+    public boolean search(String word) {
+        WordDictionary curr = this;
+        for(int i = 0; i < word.length(); ++i){
+            char c = word.charAt(i);
+            if(c == '.'){
+                for(WordDictionary ch: curr.children)
+                    if(ch != null && ch.search(word.substring(i+1))) return true;
+                return false;
             }
+            if(curr.children[c - 'a'] == null) return false;
+            curr = curr.children[c - 'a'];
         }
-        return false;
+        return curr != null && curr.isEndOfWord;
     }
 }
-
-
 /**
  * Your WordDictionary object will be instantiated and called as such:
  * WordDictionary obj = new WordDictionary();
