5267e1316c169c8695d11f85f2f69e6188e4e6c1
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-31 00:08:15 +0530

    Time: 26 ms (14.29%), Space: 47.8 MB (28.57%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.java
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.java
@@ -1,20 +1,27 @@
 class Solution {
     public String subStrHash(String str, int power, int modulo, int k, int hashValue) {
         
+        //Do dry run and you will know
+        //we are traversing from behind because dividing in modulo is not possible that's why we will reverse and multiply instead
+        
         long hash = 0;
         int n = str.length();
         int ind = n;
         long pr = 1;
         
         for(int i=n-1;i>=0;i--){
-            hash = ((hash*power)%modulo+(str.charAt(i)-'a'+1))%modulo;
+            hash = (hash*power+(str.charAt(i)-'a'+1))%modulo;
             
-            if(i+k>=n){
-                pr = (pr*power)%modulo;
+            
+            //if we have to remove or not decide here
+            
+            if(i+k>=n){ // keep updating power not removal
+                pr = pr*power%modulo;
             }
-            else{
-                hash = (hash-((str.charAt(i+k)-'a'+1)*pr)%modulo + modulo)%modulo;
+            else{ //removing
+                hash = (hash-(str.charAt(i+k)-'a'+1)*pr%modulo + modulo)%modulo;
             }
+             //ye rule hai mod minus karne ka
             
             if(hash==hashValue){
                 ind = i;
