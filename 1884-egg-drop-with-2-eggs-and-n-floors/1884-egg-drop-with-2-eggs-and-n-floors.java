b2d14d57445e2a8592996273fc1f8e452123e188
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-01-31 22:43:59 +0530

    Time: 1 ms (80.56%), Space: 40.5 MB (7.10%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
@@ -1,8 +1,6 @@
 class Solution {
     public int twoEggDrop(int n) {
         
-        int f1 = (int)Math.ceil(Math.pow(1+8*n,0.5)-1);
-        int f2 = (int)Math.ceil(((double) f1/(double) 2));
-        return f2;
+        return (int)Math.ceil((-1+Math.sqrt(1+8*n*1.0))/2);
     }
 }
