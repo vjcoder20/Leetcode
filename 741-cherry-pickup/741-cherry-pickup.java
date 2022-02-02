36b5e520cdf08fd5d7b5769f5aa08f8cad0337e5
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-02-02 09:53:49 +0530

    Time: 22 ms (62.94%), Space: 52.9 MB (30.33%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.java
+++ 741-cherry-pickup/741-cherry-pickup.java
@@ -1,7 +1,8 @@
 class Solution {
-    	public int Solution(int r1, int c1, int r2, int c2, int[][] arr,int[][][][] dp) {
+    	public int Solution(int r1, int c1, int r2,int[][] arr,int[][][] dp) {
 	    int n = arr.length;
-	    
+	    int c2 = r1+c1-r2;
+            
 	     if(r1>=n || r2>=n || c1>=n || c2>=n || arr[r1][c1]==-1 || arr[r2][c2]==-1){
 	        return Integer.MIN_VALUE;
 	    }
@@ -13,8 +14,8 @@ class Solution {
     if(r2 == n - 1 && c2 == n - 1)
       return arr[r2][c2];
             
-     if(dp[r1][c1][r2][c2]!=0){
-	        return dp[r1][c1][r2][c2];
+     if(dp[r1][c1][r2]!=0){
+	        return dp[r1][c1][r2];
 	    }
 	
 	    int cc;
@@ -24,17 +25,17 @@ class Solution {
 	    else
 	        cc =arr[r1][c1]+arr[r2][c2];
 	    
-	    int f1 = Solution(r1,c1+1,r2,c2+1,arr,dp);
-	    int f2 = Solution(r1+1,c1,r2+1,c2,arr,dp);
-	    int f3 = Solution(r1+1,c1,r2,c2+1,arr,dp);
-	    int f4 = Solution(r1,c1+1,r2+1,c2,arr,dp);
+	    int f1 = Solution(r1,c1+1,r2,arr,dp);
+	    int f2 = Solution(r1+1,c1,r2+1,arr,dp);
+	    int f3 = Solution(r1+1,c1,r2,arr,dp);
+	    int f4 = Solution(r1,c1+1,r2+1,arr,dp);
 	    
 	    cc+=Math.max(Math.max(f1,f2),Math.max(f3,f4));
 
-		return dp[r1][c1][r2][c2]=cc;
+		return dp[r1][c1][r2]=cc;
 	}
     public int cherryPickup(int[][] arr) {
         int n = arr.length;
-        return Math.max(0,Solution(0, 0, 0, 0, arr,new int[n][n][n][n]));
+        return Math.max(0,Solution(0, 0, 0,arr,new int[n][n][n]));
     }
 }
