bb5ce6a492d4227b96f409ca91c3e2bec9e25501
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-02-07 09:49:55 +0530

    Time: 2 ms (75.61%), Space: 42.7 MB (6.09%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -3,9 +3,13 @@ class Solution {
         int n = t.length();
         char ch = t.charAt(n-1);
         for(int i=0;i<n-1;i++){
-           ch^=t.charAt(i);
+           ch^=t.charAt(i);  
             ch^=s.charAt(i);
         }
         return ch;
     }
+    /*
+    c ^= s.chartAt(i);  // is same as below. 
+    c = (char) ( (int) c ^  (int) s.charAt(i));
+    */
 }
