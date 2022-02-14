2fe0af8ef6d1077483a6d2850c888341e17083fb
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-02-14 13:59:44 +0530

    Time: 2 ms (87.02%), Space: 67.4 MB (20.41%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canJump(int[] arr) {
+        /*One Approach
         int n = arr.length;
         int last = n-1;
         for(int i=n-2;i>=0;i--){
@@ -8,5 +9,13 @@ class Solution {
             }
         }
         return last==0;
+        */
+        // Second Approach
+        int reachable = 0;
+        for(int i=0;i<arr.length;i++){
+            if(i>reachable) return false;
+            reachable = Math.max(reachable,arr[i]+i);
+        }
+        return true;
     }
 }
