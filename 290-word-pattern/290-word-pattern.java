223f2e07539c7cd1519c50a942b08fa143fe30e2
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-02-22 18:58:35 +0530

    Time: 2 ms (45.89%), Space: 41.7 MB (9.23%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,46 +1,33 @@
-//--Smart Method
-class Solution {
-   public boolean wordPattern(String pattern, String str) {
-    String[] words = str.split(" ");
-    if (words.length != pattern.length())
-        return false;
-    Map index = new HashMap();
-    for (Integer i=0; i<words.length; ++i)
-        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
-            return false;
-    return true;
-}
-}
-/* -->Easy Method
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] words = s.split(" ");
-        if(words.length!=pattern.length()){
+        String[] arr = s.split(" ");
+        if(arr.length!=pattern.length()){
             return false;
         }
         HashMap<Character,String> map = new HashMap<>();
-         Map<String, Boolean> map2 = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
         
         for(int i=0;i<pattern.length();i++){
             char ch = pattern.charAt(i);
-            if(map.containsKey(ch)==false){
-                if(map2.containsKey(words[i])==true){
+            String str = arr[i];
+            
+            if(map.containsKey(ch)){
+                String get = map.get(ch);
+                if(!get.equals(str)){
                     return false;
                 }
-                else{
-                  map.put(ch,words[i]);
-                    map2.put(words[i],true);
-                }
-                
             }
-               else{
-                  String mapword = map.get(ch);
-                  if(mapword.equals(words[i])==false){
-                      return false;
-                  }
-               }
+            
+            else{
+                if(set.contains(str)){
+                    return false;
+                }
+            }
+            
+            map.put(ch,str);
+            set.add(str);
         }
+       
         return true;
     }
 }
-*/
