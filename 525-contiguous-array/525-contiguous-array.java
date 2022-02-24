9e53b7f06da4f3cefb0e54c66b98c9bc2ba584a7
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-02-24 16:22:42 +0530

    Time: 17 ms (91.88%), Space: 51.3 MB (87.44%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.java
+++ 525-contiguous-array/525-contiguous-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findMaxLength(int[] arr) {
+        /*Hard Method to think
          HashMap<Integer,Integer> map = new HashMap<>(); //c0-c1 vs first index occuring of this pair
       
       map.put(0,-1);
@@ -27,5 +28,41 @@ class Solution {
       }
 
         return oans;
+        */
+        
+        
+        //Relatively easy method to think
+        //easy method
+        
+        //replace all 0's with -1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                arr[i]=-1;
+            }
+        }
+        
+        
+        //Applying subarray having sum equal to zero
+        
+        int oval = 0;
+    HashMap<Integer,Integer> map = new HashMap<>();
+    
+    int ps = 0;
+   
+    
+    map.put(0,-1);
+    for(int i=0;i<arr.length;i++){
+        ps+=arr[i];
+        
+        if(map.containsKey(ps)){
+            int idx = map.get(ps);
+            oval = Math.max(oval,i-idx);
+        }
+        else{
+            map.put(ps,i);
+        }
+    }
+		return oval;
+        
     }
 }
