86d6d2285583662cdb6791daaeaf269cc4b6df26
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-02-24 20:46:36 +0530

    Time: 3 ms (91.34%), Space: 43.8 MB (25.50%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+     int[] arr = new int[26];
+        
+    for(int i=0;i<s.length();i++){
+        char ch = s.charAt(i);
+        arr[ch-'a']++;
+    }
+        
+  for(int i=0;i<t.length();i++){
+      char ch = t.charAt(i);
+      arr[ch-'a']--;
+  }
+    
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]!=0){
+            return false;
+        }
+    }
+    return true;
+    
+    }
+}
