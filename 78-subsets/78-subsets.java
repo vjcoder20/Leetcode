04f5ace45ad9bae6560b046847c12c5e13ff1041
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-02-26 15:05:57 +0530

    Time: 1 ms (81.48%), Space: 43.8 MB (8.42%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,18 +1,16 @@
 class Solution {
-    List<List<Integer>> li = new ArrayList<>();
-    public void fun(int[] arr,int idx,List<Integer> al){
-        if(idx==arr.length){
-            li.add(new ArrayList<>(al));
-            return;
-        }
-        
-        al.add(arr[idx]);
-        fun(arr,idx+1,al);
-        al.remove(al.size()-1);
-        fun(arr,idx+1,al);
-    }
     public List<List<Integer>> subsets(int[] arr) {
-        fun(arr,0,new ArrayList<>());
+        int n = arr.length;
+        List<List<Integer>> li = new ArrayList<>();
+        for(int mask = 0;mask<(1<<n);mask++){
+            ArrayList<Integer> al = new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if(((mask >> j) & 1)==1){
+                    al.add(arr[j]);
+                }
+            }
+            li.add(new ArrayList<>(al));
+        }
         return li;
     }
 }
