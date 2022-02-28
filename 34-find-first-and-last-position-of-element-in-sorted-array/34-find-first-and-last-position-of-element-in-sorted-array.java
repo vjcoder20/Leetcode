2699a772e1d6b7b9b8b9a3e0afd32e930eda0db3
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-01 01:22:40 +0530

    Time: 0 ms (100.00%), Space: 47.4 MB (16.50%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] searchRange(int[] arr, int x) {
-       int low = 0;
+      /* My Solution
+        int low = 0;
         int high = arr.length-1;
 
         while(low<=high){
@@ -26,5 +27,52 @@ class Solution {
         }
         }
         return new int[]{-1,-1};
+        */
+        
+        //Public Solution
+        int[] result = new int[2];
+        result[0] = firstOccurence(arr,x);
+        result[1] = lastOccurence(arr,x);
+        return result;
+    }
+    
+    public int firstOccurence(int[] arr,int x){
+        int idx = -1;
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid]>x)
+                end = mid-1;
+            
+            else if(arr[mid]<x)
+                start = mid+1;
+            
+            else{
+                idx = mid;
+                end = mid-1;
+            }
+        }
+        return idx;
+    }
+    
+     public int lastOccurence(int[] arr,int x){
+        int idx = -1;
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid]>x)
+                end = mid-1;
+            
+            else if(arr[mid]<x)
+                start = mid+1;
+            
+            else{
+                idx = mid;
+                start = mid+1;
+            }
+        }
+        return idx;
     }
 }
