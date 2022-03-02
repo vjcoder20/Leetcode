4ff90176403db7b7e826443cf2383feb4fb51160
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-02 14:11:44 +0530

    Time: 1 ms (93.29%), Space: 39.1 MB (53.09%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while(n!=1){
+            int result = 0;
+            while(n!=0){
+                result+=Math.pow(n%10,2);
+                n = n/10;
+            }
+            if(set.contains(result)){
+                return false;
+            }
+            set.add(result);
+            n = result;
+        }
+        return true;
+    }
+}
