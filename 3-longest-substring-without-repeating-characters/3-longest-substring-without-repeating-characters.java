5baee9ccbf27dde3452c1f5a6af3e507a882059d
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-02 18:45:35 +0530

    Time: 16 ms (37.82%), Space: 44.9 MB (36.21%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,46 +1,41 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
+        int i = -1;  //acquire
+        int j = -1; //release
+        
         int ans = 0;
-        int i = -1;
-        int j = -1;
-        while(true){
-        boolean f1 = false;
-        boolean f2 = false;
-         //acquire strategy
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        while(i<s.length()-1){
             
+            //acquire
             while(i<s.length()-1){
-                f1 = true;
                 i++;
                 char ch = s.charAt(i);
+                
                 map.put(ch,map.getOrDefault(ch,0)+1);
                 
-                if(map.get(ch)==2){
+                if(map.get(ch)==2)
                     break;
-                }
+                
                 else{
-                    int len = i-j;
-                    ans = Math.max(ans,len);
+                    int len = i-j;  //ans--> j+1 to i
+                  ans = Math.max(ans,len);
                 }
             }
             
-         //release strategy
-            
-        while(j<i){
-            f2 = true;
-            j++;
-            char ch = s.charAt(j);
-            map.put(ch,map.get(ch)-1);
-            
-            if(map.get(ch)==1){
-                break;
+            //release
+            while(j<i){
+                j++;
+                char ch = s.charAt(j);
+                map.put(ch,map.get(ch)-1);
+                
+                  if(map.get(ch)==1)
+                    break;
+                
             }
         }
-            
-        if(f1==false && f2==false){
-            break; //to coming out from while loop true wala
-        }
-        }
+        
         return ans;
     }
 }
