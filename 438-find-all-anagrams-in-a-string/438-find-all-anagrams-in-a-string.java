1343fc51b58f951d7949d78e0d4e9d82f2124e8e
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-06 19:02:46 +0530

    Time: 72 ms (24.09%), Space: 55.1 MB (8.97%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,48 +1,54 @@
 class Solution {
-    public List<Integer> findAnagrams(String str, String pattern) {
+    public List<Integer> findAnagrams(String s, String p) {
+        
+          //We will not do this question with pure acquire and release strategy body because we now the window size this time so we will apply common sense i.e. window size would be pattern.size   
         
         List<Integer> li = new ArrayList<>();
-        int s = str.length();
-        int p = pattern.length();
-        if(p>s) return li;
-        
-        
+        int sz = p.length();
+        int szs = s.length();
+        if(szs<sz)
+            return li;
         HashMap<Character,Integer> smap = new HashMap<>();
         HashMap<Character,Integer> pmap = new HashMap<>();
         
-        for(int i=0;i<p;i++){
-            char ch = str.charAt(i);
-            char ph = pattern.charAt(i);
-            smap.put(ch,smap.getOrDefault(ch,0)+1);
-            pmap.put(ph,pmap.getOrDefault(ph,0)+1);
-        }
+        for(char ch:p.toCharArray())
+            pmap.put(ch,pmap.getOrDefault(ch,0)+1);
         
-        int i = p;
-
-        while(i<s){
-            
-            if(smap.equals(pmap)){
-                li.add(i-p);
-            }
-            
-            char ch = str.charAt(i);
+        int i = 0;
+        int j = 0;
+        
+        while(i<sz){
+            char ch = s.charAt(i);
             smap.put(ch,smap.getOrDefault(ch,0)+1);
-            
-            char chr = str.charAt(i-p);
-            if(smap.get(chr)==1){
-                smap.remove(chr);
-            }
-            else{
-               smap.put(chr,smap.get(chr)-1); 
-            }
-            
             i++;
         }
-        System.out.println(smap);
-         if(smap.equals(pmap)){
-                li.add(i-p);
-            }
         
+        
+        
+        while(i<s.length()){
+               if(smap.equals(pmap))
+                    li.add(j);
+            
+            char chi = s.charAt(i);
+            char chj = s.charAt(j);
+            
+            //acquire
+              smap.put(chi,smap.getOrDefault(chi,0)+1);
+            
+            //release
+            int freq = smap.get(chj);
+            if(freq==1)
+            smap.remove(chj);
+            
+            else
+             smap.put(chj,smap.get(chj)-1);
+            
+            i++;
+            j++;
+        }
+       
+         if(smap.equals(pmap))
+                    li.add(j);
         
        
         return li;
