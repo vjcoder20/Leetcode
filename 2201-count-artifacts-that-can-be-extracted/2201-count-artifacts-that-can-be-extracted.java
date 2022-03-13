cb6eaf74c2d3ed78d0d03a9fc1431439bb711365
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-13 22:38:22 +0530

    Time: 6 ms (100.00%), Space: 131.7 MB (14.29%) - LeetHub

---
+++ 2201-count-artifacts-that-can-be-extracted/2201-count-artifacts-that-can-be-extracted.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int digArtifacts(int n, int[][] artifacts, int[][] dig) {
+        boolean[][] visited = new boolean[n][n];
+        for(int i=0;i<dig.length;i++){
+          visited[dig[i][0]][dig[i][1]] = true;
+        }
+        int count = 0;
+        for(int[] arr:artifacts){
+            boolean done = true;
+            for(int i=arr[0];i<=arr[2];i++){
+                for(int j=arr[1];j<=arr[3];j++){
+                    if(visited[i][j]==false)
+                        done = false;
+                }
+            }
+            if(done==true) count++;
+        }
+        // System.out.println(Arrays.deepToString(visited));
+        return count;
+    }
+}
