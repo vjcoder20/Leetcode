797825bd3fe3798c565fb8d9d77e7baeefd68f7e
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-23 20:27:59 +0530

    Time: 0 ms (100.00%), Space: 50.1 MB (30.09%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    /*
     int count = 0;
     public void preorder(TreeNode root){
         if(root==null)
@@ -23,8 +24,40 @@ class Solution {
         preorder(root.left);
         preorder(root.right);
     }
+    */ 
     public int countNodes(TreeNode root) {
+         /*O(n) solution
         preorder(root);
+        return count;
+        */
+        //O(Log^2 N) solution
+         if(root==null)
+             return 0;
+        
+        int lh = getLeftHeight(root);  //lh-->left height
+        int rh = getRightHeight(root); //rh-->right height
+        
+        if(lh==rh)
+            return ((1<<(lh))-1);
+        
+        else
+        return countNodes(root.left) + countNodes(root.right) + 1;  
+    }
+    
+    public int getLeftHeight(TreeNode root){
+        int count = 0;
+        while(root!=null){
+            count++;
+            root = root.left;
+        }
+        return count;
+    }
+    public int getRightHeight(TreeNode root){
+        int count = 0;
+        while(root!=null){
+            count++;
+            root = root.right;
+        }
         return count;
     }
 }
