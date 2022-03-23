7f79b44340a550639cab5635b0e941eb8285c8c8
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-23 15:34:11 +0530

    Time: 0 ms (100.00%), Space: 41 MB (30.35%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -1,20 +1,19 @@
 class Solution {
     public int minMoves(int target, int maxDoubles) {
         int ans = 0;
-                while(target>1){
-            if(target%2==0 && maxDoubles>0){
-                target = target/2;
-                maxDoubles-=1;
+        while(target>1 && maxDoubles>0){
+            if(target%2==0){
+                maxDoubles--;
+                target/=2;
             }
-            else if(maxDoubles==0){
-               ans+=target-1;
-                break;
-            }
-            else{
-                target--;
-            }
-         ans++;
+            else
+            target--;
+            
+            ans++;
         }
+        if(target>1)
+            return ans+target-1;
+        
         return ans;
     }
 }
