b471729f66524c295ca5d8599e35cc9c7630f67d
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-25 23:15:03 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (11.61%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -17,18 +17,27 @@ class Solution {
     int cam = 0;
     public int minCameraCover(TreeNode root) {
         int rs = helper(root);
+        
+        //It may be the case where the root itself need camera
         if(rs==0)
         return cam+1;
         
         return cam;
     }
     
+    /*
+    Some points to know before moving ahead for this question
+    0--> Needy i.e. it needs to be covered
+    1--> Have camera on itself
+    2--> Already covered no need of camera like (null node)
+    */
+    
     public int helper(TreeNode node){
         if(node==null)
             return 2;
         
-        int lci = helper(node.left);
-        int rci = helper(node.right);
+        int lci = helper(node.left);  //left child indication
+        int rci = helper(node.right); //right child indication
         
         if(lci==0 || rci==0){
             cam++;
