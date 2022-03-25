4e2323d7b9b5f9a99c5af70cd9d65352ff0ea883
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-25 18:10:46 +0530

    Time: 1 ms (99.54%), Space: 40.5 MB (80.59%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -2,14 +2,21 @@ class Solution {
     public int twoCitySchedCost(int[][] arr) {
         int n = arr.length;
         int ans = 0;
+        
+        //Here the greedy factor is loss visiting city B over city A            i.e. costA - costB
+        
+        //negative value simply means it is valuable to choose cityA           over cityB and vice versa
+        
         Arrays.sort(arr, (a, b) -> {
             return (a[0] - a[1]) - (b[0] - b[1]);
         }); 
         
-        System.out.println(Arrays.deepToString(arr));
+        // System.out.println(Arrays.deepToString(arr));
+       
         for(int i=0;i<n;i++){
             ans+=i<n/2?arr[i][0]:arr[i][1];
-        }
+        }      
+        
         return ans;
     }
 }
