6d0083dd4be84a96ec3b8613d8cbe6cd06164fc0
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-28 15:27:09 +0530

    Time: 1 ms (81.31%), Space: 44.5 MB (6.43%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,37 @@
 class Solution {
     public boolean search(int[] arr, int target) {
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==target)
+        int low = 0;
+        int high = arr.length-1;
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            
+            if(arr[mid]==target){
                 return true;
+            }
+            
+            //Ex: [1,1,1,1,1,2,1,1,1]  and mid = 4 and target = 2
+            if((arr[mid]==arr[high]) && (arr[mid]==arr[low])){
+                low++;
+                high--;
+            }
+            
+            //Ex: [4,5,6,1,2,2,3,4,4]  mid = 4 and target = 3
+            else if(arr[mid]<=arr[high]){   //to check if 'right' part contains target
+            if((arr[mid]<target) && (arr[high]>=target))  //right part of array is sorted
+                low = mid+1;
+                else                           //left part of array is sorted
+                high = mid-1;
+            }
+            
+            else{  //to check if 'left' contains target
+                if((arr[mid]>target) && (arr[low]<=target))
+                    high = mid-1;
+                else
+                    low = mid+1;
+            }
         }
+        
         return false;
     }
 }
