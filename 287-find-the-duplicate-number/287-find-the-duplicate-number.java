acf24dbe41d4f1b28756addcfed74d04673221d5
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-03-29 10:19:54 +0530

    Time: 43 ms (18.04%), Space: 76.6 MB (22.23%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findDuplicate(int[] arr) {
+        /*Method 1  O(n) tc and O(n) sc
         HashMap<Integer,Integer> map = new HashMap<>();
         int ans = 0;
         for(int elem:arr){
@@ -7,6 +8,16 @@ class Solution {
                 map.put(elem,1);
             else{
                 ans = elem;
+                break;
+            }
+        }
+        return ans;
+        */
+        int ans = 0;
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+               ans = arr[i];
                 break;
             }
         }
