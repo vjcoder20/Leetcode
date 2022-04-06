cc42a63d6518c7864862556f00bdfaeb63b81a6a
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-04-06 13:27:01 +0530

    Time: 4 ms (80.65%), Space: 44 MB (55.65%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -47,18 +47,25 @@ class Solution {
         for (int i = 0; i <= max; i++)
             for (int j = i; j <= max; j++) {
                 
-                int k = target - i - j;
+                int k = target - i - j;  //as (i+j+k==target)
                 
-                if (k > 100 || k < 0) continue;
+                if(k<0 || k>100)
+                    continue;
                 
-                if (i == j && j == k)  //nC3
-                    ans += freq[i] * (freq[i] - 1) * (freq[i] - 2) / 6;
+    //nC3 (as all same numbers able to meet the target Ex: arr [1,1,1,1,1,1,1] , target=3) Case:{1,1,1} 
+                if (i == j && j == k)  
+                    ans += freq[i] * (freq[i] - 1) * (freq[i] - 2) / 6;  //after simplfying nc3 you will get this
                 
-                else if (i == j && j != k)  //nC2
-                    ans += freq[i] * (freq[i] - 1) / 2 * freq[k];
+ //nC2 (as one number is different and 2 are same in contributing to achieve the target Ex: [1,1,1,1,1,2,2,2,2], target = 5) Case:{1,2,2}
                 
-                else if (j < k)  //nC1
-                    ans += freq[i] * freq[j] * freq[k];
+                else if (i == j && j != k)   
+            ans += freq[i] * (freq[i] - 1) / 2 * freq[k];    //after simplfying nc2 you will get this * different ones freq have to multiply
+                
+//nC1 (as all number are different in contributing to achieving the target Ex: [1,1,1,1,1,3,3,3,4,4,4,4], target=8)  Case:{1,3,4}
+//also why i am writing this condition (i<j && j < k) because so that you don't add more answers for cases like permuation of {2,3,4} -> {2,4,3} you just have to add once only           
+
+                else if (i<j && j < k)    
+                    ans += freq[i] * freq[j] * freq[k];   //after simplfying nc1 you will get this
             }
         
         return (int)(ans % (1e9 + 7));
