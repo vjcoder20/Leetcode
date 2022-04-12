9234e4a9c41337bc16a7e3ac6c574561b1addda3
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-04-12 20:31:32 +0530

    Added solution - LeetHub

---
+++ Mother Vertex - GFG/mother-vertex.java
@@ -0,0 +1,87 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for(int i = 0; i < V; i++)
+                adj.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+            }
+            Solution obj = new Solution();
+            int ans = obj.findMotherVertex(V, adj);
+            System.out.println(ans);
+       }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to find a Mother Vertex in the Graph.
+    public int findMotherVertex(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+       Stack<Integer> st = new Stack<>();
+       boolean[] visited = new boolean[V];
+       
+       for(int i=0;i<V;i++){
+           if(visited[i]==false){
+               dfs(adj,i,visited,st);
+           }
+       }
+       
+       int pmv = st.peek();  //potential mother vertex
+       visited = new boolean[V];
+       
+       dfs(adj,pmv,visited);
+       
+       for(int i=0;i<V;i++){
+           if(visited[i]==false){
+               return -1;
+           }
+       }
+       
+       return pmv;
+       
+    }
+    
+    public void dfs(ArrayList<ArrayList<Integer>>adj,int src,boolean[] visited,Stack<Integer> st){
+        
+        visited[src] = true;
+        
+        for(int nbr:adj.get(src)){
+            if(visited[nbr]==false)
+            dfs(adj,nbr,visited,st);
+        }
+        
+        st.push(src);
+    }
+    
+     public void dfs(ArrayList<ArrayList<Integer>>adj,int src,boolean[] visited){
+        
+        visited[src] = true;
+        
+        for(int nbr:adj.get(src)){
+            if(visited[nbr]==false)
+            dfs(adj,nbr,visited);
+        }
+        
+     
+    }
+}
