f8447637b7d2b9adb5a7fd268c642d9529d95de5
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-04-15 18:22:03 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (79.62%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findPeakElement(int[] arr) {
-        //Naive Approach
+        /*Naive Approach --> O(n)
         if(arr.length==1){
             return 0;
         }
@@ -21,5 +21,46 @@ class Solution {
             }
         }
         return -1;
+        */
+        //Optimal approach O(logn)
+          if(arr.length==1){
+            return 0;
+        }
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            
+            if(mid>0 && mid<n-1){
+                if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                    return mid;
+                }
+                
+                else if(arr[mid]>arr[mid-1]){
+                    low = mid+1;
+                }
+                
+                else
+                high = mid-1;
+            }
+            
+            else if(mid==0){
+                if(arr[mid]>arr[1])
+                    return mid;
+                else
+                return 1;
+            }
+            
+            else if(mid==n-1){
+                if(arr[n-1]>arr[n-2])
+                    return mid;
+                else
+                return n-2;
+            }
+        }
+        return -1;
+        
     }
 }
