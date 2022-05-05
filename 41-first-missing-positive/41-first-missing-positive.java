f88559093377ac682d2ebcdee4018647c6729094
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-05-05 19:31:25 +0530

    Time: 4 ms (60.31%), Space: 127.5 MB (43.62%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int firstMissingPositive(int[] arr) {
+    public int firstMissingPositive(int[] nums) {
+        /* O(n) time complexity and O(n) Space Complexity
         HashSet<Integer> set = new HashSet<>();
         for(int i=0;i<arr.length;i++){
             set.add(arr[i]);
@@ -10,5 +11,32 @@ class Solution {
             temp++;
         }
         return temp;
+        */
+        //O(n) time complexity and O(1) Space Complexity
+         //put Integer.MAX_VALUE in place of negative numbers or 0
+        for(int i=0; i < nums.length;i++) {
+            if(nums[i] <= 0) {
+                nums[i] = Integer.MAX_VALUE;
+            }
+        }
+        
+        //marking
+        for(int i=0; i < nums.length;i++) {
+            int val = Math.abs(nums[i]);
+            
+            if(val-1 < nums.length && nums[val-1] > 0) {
+                nums[val-1] = -nums[val-1];
+            }
+        }
+        
+        
+        //find your ans - the first unmarked spot
+        for(int i=0; i < nums.length;i++) {
+            if(nums[i] > 0) {
+                return i+1;
+            }
+        }
+        
+        return nums.length+1;
     }
 }
