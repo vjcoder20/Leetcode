e38129493e2f8d3605e5d11765a8cfa321ed84ac
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-05-09 16:29:15 +0530

    Time: 8 ms (26.54%), Space: 43.3 MB (10.26%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,30 @@
 class Solution {
      String[] arr = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    public void kpc(String str,String asf,List<String> li){
-         if(str.length()==0){
-            li.add(asf);
-            return;
+     public ArrayList<String> getKPC(String str) {
+        if(str.length()==0){
+            ArrayList bans = new ArrayList<>();
+            bans.add("");
+            return bans;
         }
         char ch = str.charAt(0);
         String ros = str.substring(1);
+        ArrayList<String> rres = getKPC(ros);
+        ArrayList<String> mres = new ArrayList<>();
+        
         String codeforch = arr[ch-'0'];
-       for(int i=0;i<codeforch.length();i++){
-           char chp = codeforch.charAt(i);
-           kpc(ros,asf+chp,li);
-       }  
+        for(int i=0;i<codeforch.length();i++){
+            char chcode = codeforch.charAt(i);
+            for(int j =0;j<rres.size();j++){
+                mres.add(chcode + rres.get(j));
+            }
+        }
+        return mres;
     }
     public List<String> letterCombinations(String digits) {
-        List<String> li = new ArrayList<>();
+       
         if(digits.length()==0){
-            return li;
+            return new ArrayList<>();
         }
-        kpc(digits,"",li);
-        return li;
+        return getKPC(digits);
     }
 }
