dc9023901b6a1f1da1b0a2ac5385d292c34142f1
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-05-17 23:40:14 +0530

    Time: 34 ms (48.82%), Space: 59.3 MB (39.68%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<List<Integer>> twoSum(int[] arr,int idx, int target) {
+   List<List<Integer>> ans = new ArrayList<>();
+   
+   int i = idx;
+   int j = arr.length-1;
+   
+   while(i<j){
+       if(i>idx && arr[i]==arr[i-1]){  //to avoid duplicates
+           i++;
+           continue;
+       }
+       
+       if(arr[i]+arr[j]==target){
+           ArrayList<Integer> pair = new ArrayList<>();
+           pair.add(arr[i]);
+           pair.add(arr[j]);
+           ans.add(pair);
+           i++;
+           j--;
+       }
+       else if(arr[i]+arr[j]<target){
+           i++;
+       }
+       else
+       j--;
+   }
+   
+   return ans;
+  }
+    public List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+        int target = 0;
+        
+        //mein basically target sum triplet kar raha hu
+        
+        for(int i=0;i<arr.length-2;i++){
+            if(i-1>=0 && arr[i-1]==arr[i]){
+                continue;
+            }
+           
+            List<List<Integer>> temp = twoSum(arr,i+1,target-arr[i]);
+            
+            for(int k=0;k<temp.size();k++){
+                temp.get(k).add(arr[i]);
+                ans.add(temp.get(k));
+            }
+                
+            }
+        return ans;
+        }
+    }
