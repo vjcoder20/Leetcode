48f9f234c2834d90bbd12ea50d0e39e860ccac22
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-05-20 11:05:10 +0530

    Time: 1 ms (52.25%), Space: 42.6 MB (23.61%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -17,29 +17,5 @@ class Solution {
         }
         return dp[m-1][n-1];
     }
-     public int getPath(int[][] arr,int sr,int sc,int[][] dp){
-          
-         if(sr>=0 && sc>=0 && arr[sr][sc]==1){
-             return 0;
-         }
-         
-        if(sr==0&&sc==0){
-            return 1;
-        }
-         if(sr<0 || sc<0){
-             return 0;
-         }
-        
-        if(dp[sr][sc]!=0){
-            return dp[sr][sc];
-        }
-         
-       
-        int rowa = getPath(arr,sr-1,sc,dp);
-        int cola = getPath(arr,sr,sc-1,dp);
-    
-        
-        dp[sr][sc] = rowa+cola;
-        return (rowa+cola);
-    }
+   
 }
