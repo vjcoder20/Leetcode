9e00ea367e22b3500d0d4d8629037f92533c7bce
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-08-06 17:00:18 +0530

    Time: 1 ms (99.10%), Space: 42.3 MB (86.67%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -14,11 +14,15 @@
  * }
  */
 class Solution {
+    boolean temp = true;
     public int height(TreeNode root){
         if(root==null) return 0;
         
         int lh = height(root.left);
         int rh = height(root.right);
+        
+        if(Math.abs(lh-rh)>1)
+            temp = false;
         
         return 1+Math.max(lh,rh);
     }
@@ -27,9 +31,8 @@ class Solution {
        if(root==null)
            return true;
         
-        int lh = height(root.left);
-        int rh = height(root.right);
+        height(root);
+        return temp;
         
-        return Math.abs(lh-rh)<=1 && isBalanced(root.left) && isBalanced(root.right);
     }
 }
