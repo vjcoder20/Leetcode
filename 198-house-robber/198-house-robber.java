de5d59654fdc57df831f98fb8aceb8ce1df73919
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-08-10 23:16:38 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (85.49%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,32 +1,19 @@
 class Solution {
-    public int maxSum(int[] arr,int idx,int[] dp){
-        
-        if(idx==0)
-            return arr[idx];
-        
-        if(idx<0) return 0;
-        
-        if(dp[idx]!=-1)
-            return dp[idx];
-            
-            int pick = arr[idx]+maxSum(arr,idx-2,dp);
-            int notpick = maxSum(arr,idx-1,dp);
-        
-        return dp[idx]=Math.max(pick,notpick);
-        
-    }
-    
+  
     public int rob(int[] arr) {
-        int[] dp = new int[arr.length];
-        
-        dp[0] = arr[0];
+       
+        int prev = arr[0];
+        int prev2 = 0;
         
         for(int i=1;i<arr.length;i++){
-            int pick = arr[i]+(i>1?dp[i-2]:0);
-            int notpick = dp[i-1];
-            dp[i] = Math.max(pick,notpick);
+            int pick = arr[i]+(i>1?prev2:0);
+            int notpick = prev;
+            int curr = Math.max(pick,notpick);
+            
+            prev2 = prev;
+            prev = curr;
         }
         
-       return dp[arr.length-1];
+       return prev;
     }
 }
