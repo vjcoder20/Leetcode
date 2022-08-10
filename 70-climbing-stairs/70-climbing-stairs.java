d9815f3b27753bbed34def61826dc2e877dc1a5d
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-08-10 14:28:18 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (90.03%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,9 +1,9 @@
 class Solution {
     public int climbStairs(int n) {
-        int a = 0;
+        int a = 1;
         int b = 1;
         
-        for(int i=1;i<=n;i++){
+        for(int i=2;i<=n;i++){
           int c = a+b;
           a = b;
           b = c;
