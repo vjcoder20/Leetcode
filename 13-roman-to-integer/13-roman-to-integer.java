4e850d0dbf2031a49ca2fbfb8f3216500f93e930
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-08-15 10:59:29 +0530

    Time: 11 ms (45.05%), Space: 46.8 MB (29.88%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int romanToInt(String s) {
-         HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character,Integer> map = new HashMap<>();
         map.put('I',1);
         map.put('V',5);
         map.put('X',10);
@@ -9,37 +9,17 @@ class Solution {
         map.put('D',500);
         map.put('M',1000);
         
-        int ans = map.get(s.charAt(0));
+        int n = s.length();
         
-        for(int i=1;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='V' || ch=='X'){
-                char ch2 = s.charAt(i-1);
-                if(ch2=='I'){
-                    ans+=map.get(ch)-2*map.get(ch2);
-                }
-                else
-                ans+=map.get(ch);
-            }
-            else if(ch=='L' || ch=='C' ){
-                 char ch2 = s.charAt(i-1);
-                if(ch2=='X'){
-                     ans+=map.get(ch)-2*map.get(ch2);
-                }
-                else
-                ans+=map.get(ch); 
-            }
-            else if(ch=='D' || ch=='M'){
-                 char ch2 = s.charAt(i-1);
-                if(ch2=='C'){
-                    ans+=map.get(ch)-2*map.get(ch2);
-                }
-                 else
-                ans+=map.get(ch);
-            }
+        int ans = map.get(s.charAt(n-1));
+        
+        for(int i=n-2;i>=0;i--){
+            if(map.get(s.charAt(i))<map.get(s.charAt(i+1)))
+                ans-=map.get(s.charAt(i));
             else
-            ans+=map.get(ch);
+            ans+=map.get(s.charAt(i));
         }
+      
         
         return ans;
         
