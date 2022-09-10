e7f9c0069bc39aea898ea6f126abdac5d6b09526
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-09-11 01:37:23 +0530

    Time: 3 ms (19.75%), Space: 44.3 MB (46.58%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,31 +1,12 @@
 class Solution {
-    public int solve(int[] arr,int idx,int buy,Integer[][] dp){
-        
-        if(idx==arr.length)
-            return 0;
-        
-        if(dp[idx][buy]!=null)
-            return dp[idx][buy];
-        
-        int profit = 0;
-        
-        if(buy==1){
-            int profit1 = -arr[idx]+solve(arr,idx+1,0,dp); //take
-            int profit2 = 0 + solve(arr,idx+1,1,dp);  //not take
-            profit = Math.max(profit1,profit2);
-        }
-        else{  //means sell
-            int profit1 = arr[idx]+solve(arr,idx+1,1,dp); //take
-            int profit2 = 0 + solve(arr,idx+1,0,dp);  //not take
-            profit = Math.max(profit1,profit2);
-        }
-            
-        
-        return dp[idx][buy]=profit;
-    }
-    
     public int maxProfit(int[] arr) {
-        Integer[][] dp = new Integer[arr.length][2];
-        return solve(arr,0,1,dp);  //here one signify buy and 0 is to not buy
+        int n = arr.length;
+         int maxProfit = 0;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]<arr[i+1]){
+                maxProfit+=arr[i+1]-arr[i];
+            }
+        }
+        return maxProfit;
     }
 }
