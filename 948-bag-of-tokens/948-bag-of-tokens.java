4f28ccecac39584fb7f03395bcf232b54388888a
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-09-12 10:15:01 +0530

    Time: 4 ms (60.64%), Space: 43.8 MB (19.68%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.java
+++ 948-bag-of-tokens/948-bag-of-tokens.java
@@ -20,8 +20,7 @@ class Solution {
                 high--;
             }
             else{
-                low++;
-                high--;
+                break;
             }
         }
         
