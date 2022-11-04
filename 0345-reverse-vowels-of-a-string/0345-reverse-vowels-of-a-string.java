e431e750aa4089bad12aa43b0cd37776c4418779
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-11-04 12:46:06 +0530

    Time: 7 ms (67.00%), Space: 45 MB (61.45%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,36 +1,43 @@
 class Solution {
-    public void swap(StringBuilder sb,int i,int j){
-        char chi = sb.charAt(i);
-        char chj = sb.charAt(j);
-        sb.setCharAt(i,chj);
-        sb.setCharAt(j,chi);
+    public boolean vowel(char ch){
+        return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A'||ch=='E' || ch=='I' || ch=='O' || ch=='U');
     }
+    
     public String reverseVowels(String s) {
-        int i = 0;
-        int j = s.length()-1;
+        
         StringBuilder sb = new StringBuilder(s);
         
-       while(i<j){
-           char chi = sb.charAt(i);
-           char chj = sb.charAt(j);
-           
-           if((chi=='a' || chi=='e' || chi=='i' || chi=='o' || chi=='u' || chi=='A' || chi=='E' || chi=='I' || chi=='O' || chi=='U') && (chj=='a' || chj=='e' || chj=='i' || chj=='o' || chj=='u' || chj=='A' || chj=='E' || chj=='I' || chj=='O' || chj=='U')){
-           swap(sb,i,j);
-           i++;j--;
-           }
-           
-           else if(chi=='a' || chi=='e' || chi=='i' || chi=='o' || chi=='u'|| chi=='A' || chi=='E' || chi=='I' || chi=='O' || chi=='U'){
-               j--;
-           }
-           
-           else if(chj=='a' || chj=='e' || chj=='i' || chj=='o' || chj=='u' || chj=='A' || chj=='E' || chj=='I' || chj=='O' || chj=='U' ){
-               i++;
-           }
-           
-           else{
-               i++;j--;
-           }
-       }
+        
+        int i = 0;
+        int j = sb.length()-1;
+        
+        while(i<j){
+            char chi = sb.charAt(i);
+            char chj = sb.charAt(j);
+            
+            if(vowel(chi)==true && vowel(chj)==true){
+                sb.setCharAt(i,chj);
+                sb.setCharAt(j,chi);
+                i++;
+                j--;
+            }
+            
+            else if(vowel(chi)==true){
+                j--;
+            }
+            
+            else if(vowel(chj)==true){
+                i++;
+            }
+            
+            else{
+                i++;
+                j--;
+            }
+        }
+        
+       
         return sb.toString();
+        
     }
 }
