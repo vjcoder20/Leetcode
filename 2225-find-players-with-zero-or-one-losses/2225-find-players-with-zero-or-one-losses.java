9ed809d1ca5a73b0bad7b18029d0c4815d5a9a40
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-11-28 15:45:45 +0530

    Time: 154 ms (75.61%), Space: 152.3 MB (52.06%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,39 +1,44 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] arr) {
         List<List<Integer>> li = new ArrayList<>();
+        
+        
         HashMap<Integer,Integer> map = new HashMap<>();
-        HashMap<Integer,Integer> map2 = new HashMap<>();
+        
         
       
-        
-        
-        ArrayList<Integer> wi = new ArrayList<>();
-         ArrayList<Integer> lo = new ArrayList<>();
-        
-        
         for(int i=0;i<arr.length;i++){
-            map.put(arr[i][0],map.getOrDefault(arr[i][0],0)+1);
-            map2.put(arr[i][1],map2.getOrDefault(arr[i][1],0)+1);
+            int win = arr[i][0];
+            int lose = arr[i][1];
+            
+            map.put(win,map.getOrDefault(win,0));
+            map.put(lose,map.getOrDefault(lose,0)+1);
+            
         }
         
+      List<Integer> win = new ArrayList<>();
+      List<Integer> lose = new ArrayList<>();
+        
+       
         
         for(int key:map.keySet()){
-            if(map2.containsKey(key)==false)
-              wi.add(key);
+            int freq = map.get(key);
+           
+            if(freq==0)
+              win.add(key); 
+            
+            
+            else if(freq==1)
+             lose.add(key);  
+            
         }
         
-        Collections.sort(wi);
-        li.add(wi);
-        
-        
-        for(int key:map2.keySet()){
-            if(map2.get(key)==1)
-               lo.add(key);
-        }
-        
-         Collections.sort(lo);
-        li.add(lo);
-        
+        Collections.sort(win);
+        Collections.sort(lose);
+
+
+          li.add(win);
+          li.add(lose);
         
         
         return li;
