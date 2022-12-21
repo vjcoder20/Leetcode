c80a8318b4019247be6d969a33a1fc0ae4752803
Vaibhav Jain <jain.vaibhav020@gmail.com>
2022-12-22 01:00:55 +0530

    Time: 16 ms (90.61%), Space: 49.8 MB (94.30%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,30 +1,81 @@
 class Solution {
-    public boolean possibleBipartition(int N, int[][] dislikes) {
-        int[][] graph = new int[N][N];
-        for (int[] d : dislikes) {
-            graph[d[0] - 1][d[1] - 1] = 1;
-            graph[d[1] - 1][d[0] - 1] = 1;
+    
+     static class Edge {
+      int src;
+      int nbr;
+
+      Edge(int src, int nbr) {
+         this.src = src;
+         this.nbr = nbr;
+      }
+         
+   }
+     static class Pair{
+        int vtx;
+        int lvl;
+        
+        Pair(int vtx,int lvl){
+            this.vtx = vtx;
+            this.lvl = lvl;
         }
-        int[] group = new int[N];
-        for (int i = 0; i < N; i++) {
-            if (group[i] == 0 && !dfs(graph, group, i, 1)) {
-                return false;
-            }
-        }
-        return true;
     }
-    public boolean dfs(int[][] graph, int[] group, int index, int g) {
-        group[index] = g;
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[index][i] == 1) {
-                if (group[i] == g) {
-                    return false;
-                }
-                if (group[i] == 0 && !dfs(graph, group, i, -g)) {
-                    return false;
-                }
-            }
+    
+    
+    public boolean possibleBipartition(int n, int[][] arr) {
+         ArrayList<Edge>[] graph = new ArrayList[n+1];
+          for (int i = 0; i <= n; i++) {
+         graph[i] = new ArrayList<>();
+      }
+      
+        for(int i=0;i<arr.length;i++){
+         int v1 = arr[i][0];
+         int v2 = arr[i][1]; 
+         graph[v1].add(new Edge(v1, v2));
+         graph[v2].add(new Edge(v2,v1));
         }
+        
+         int[] visited = new int[n+1];
+        Arrays.fill(visited,-1);
+        
+        for(int i=1;i<=n;i++){
+          if(visited[i]==-1){
+              boolean ans = isBipartite(graph,i,visited);
+              if(ans==false){
+                 return false;
+              }
+          }
+      }
+       
         return true;
+        
     }
+    
+    
+    public boolean isBipartite(ArrayList<Edge>[] graph,int src,int[] visited){
+     Queue<Pair> q = new ArrayDeque<>();
+      q.add(new Pair(src,0));
+     
+      while(q.size()>0){
+          
+          Pair rp = q.remove();
+          if(visited[rp.vtx]!=-1){
+              if(rp.lvl!=visited[rp.vtx])
+                  return false;
+          }
+          
+          else{
+             visited[rp.vtx] = rp.lvl;
+             for(Edge e:graph[rp.vtx]){
+                  int nbr = e.nbr;
+                  if(visited[nbr]==-1)
+                      q.add(new Pair(nbr,rp.lvl+1));
+          }
+      }
+      
+      
+}
+   
+        return true;
+    
+}
 }
