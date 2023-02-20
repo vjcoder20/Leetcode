aad6c2f92f881c43a97d0fa978e32dc29f584946
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-02-21 00:08:14 +0530

    Time: 1 ms (100.00%), Space: 51.3 MB (93.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,15 +1,45 @@
 class Solution {
     public int maxSubArray(int[] arr) {
+
+        //int si = 0;     starting index
+       // int ei = 0;     ending index
+        
+ /*
+ Here i have also find starting and ending index of maxSubArray
+ to make question more challenging
+ */
         
         int currsum = arr[0];
         int maxsum = arr[0];
         
         for(int i=1;i<arr.length;i++){
+            
+            /*
+            if(currsum+arr[i]<arr[i]){
+                si = i;
+            }
+            */
+            
             currsum = Math.max(currsum+arr[i],arr[i]);
-            maxsum = Math.max(maxsum,currsum);
+            
+            /*
+            
+            if(maxsum<currsum){
+            ei = i;
+            }
+            
+            */
+            
+             maxsum = Math.max(maxsum,currsum);
         }
         
-        return maxsum;
+      
         
+        // System.out.println(si);
+        // System.out.println(ei);
+        
+         return maxsum;
+        }
+        
+       
     }
-}
