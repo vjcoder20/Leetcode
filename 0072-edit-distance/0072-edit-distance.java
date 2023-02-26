22841e1733e310362bdbc1911d536ae5bcb184f7
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-02-27 00:16:33 +0530

    Time: 6 ms (67.73%), Space: 42.6 MB (41.57%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minDistance(String str1, String str2) {
+        	int[][] dp = new int[str1.length()+1][str2.length()+1];
+		
+		for(int i=0;i<dp.length;i++){
+		    for(int j=0;j<dp[0].length;j++){
+		        if(i==0)
+		        dp[i][j] = j;
+		    
+		        else if(j==0)
+		        dp[i][j] = i;
+		    
+		        else{
+		        if(str1.charAt(i-1)==str2.charAt(j-1))
+		        dp[i][j] = dp[i-1][j-1];
+		    
+		        else
+		        dp[i][j] = 1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+		    }
+		}
+		}
+		
+	return dp[dp.length-1][dp[0].length-1];
+    }
+}
