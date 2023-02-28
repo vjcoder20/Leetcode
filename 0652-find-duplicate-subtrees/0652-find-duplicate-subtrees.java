51f06e8a7b201d6b71c238dc3cd1941d934d245c
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-03-01 01:36:40 +0530

    Time: 19 ms (73.68%), Space: 48.9 MB (59.26%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -22,7 +22,7 @@ class Solution {
 
 public String postorder(TreeNode cur, Map<String, Integer> map, List<TreeNode> res) {
     if (cur == null) return "#";  
-    String serial = cur.val + "," + postorder(cur.left, map, res) + "," + postorder(cur.right, map, res);
+    String serial = cur.val + "," + postorder(cur.left, map, res) + "," +postorder(cur.right, map, res);
     map.put(serial, map.getOrDefault(serial, 0) + 1);
     if (map.get(serial) == 2) res.add(cur);
     return serial;
