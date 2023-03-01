5df47ebcd6445d2f038bd15d00cd7d85a369ddf0
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-03-02 01:29:47 +0530

    Time: 23 ms (83.88%), Space: 53.1 MB (49.32%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,8 +1,60 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
+    public int[] sortArray(int[] arr) {
         
-        Arrays.sort(nums);
-        return nums;
+        return mergeSort(arr,0,arr.length-1);
         
     }
+    
+    public int[] mergeSort(int[] arr,int low,int high){
+        
+        if(low==high)
+            return new int[]{arr[low]};
+        
+        int mid = low+(high-low)/2;
+        
+       int[] left = mergeSort(arr,low,mid);
+       int[] right = mergeSort(arr,mid+1,high);
+        
+        int[] merged = merge2SortedArrays(left,right);
+        
+        return merged;
+        
+    }
+    
+    public int[] merge2SortedArrays(int[] left,int[] right){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        int[] merged = new int[left.length+right.length];
+        
+        while(i<left.length && j<right.length){
+            if(left[i]<=right[j]){
+                merged[k] = left[i];
+                k++;
+                i++;
+            }
+            
+            else{
+                merged[k] = right[j];
+                k++;
+                j++;
+            }
+        }
+        
+         while(i<left.length){
+             merged[k] = left[i];
+             k++;
+             i++;
+         }
+         
+          while(j<right.length){
+             merged[k] = right[j];
+             k++;
+             j++;
+         }
+        
+        
+        return merged;
+    } 
 }
