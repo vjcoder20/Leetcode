24cc576685b72d471e00bb91a4ddbf4390b88791
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-03-25 13:44:54 +0530

    Time: 3 ms (39.95%), Space: 44.2 MB (10.07%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,31 +1,53 @@
 class Solution {
+    public void dfs(int src,List<List<Integer>> li,boolean[] visited){
+        
+        visited[src] = true;
+        
+        for(int elem:li.get(src)){
+            if(visited[elem]==false)
+                dfs(elem,li,visited);
+        }
+        
+    }
+    
     public int findCircleNum(int[][] arr) {
         
-        boolean[] visited = new boolean[arr.length];
+        /*
+        Firstly I need to convert adjacency matrix to adjacency list so I can
+        easily play,although It can be done easily with the matrix also
+        */
+        
+        int n = arr.length;
         int count = 0;
         
-        for(int i=0;i<arr.length;i++){
+        
+        List<List<Integer>> li = new ArrayList<>();
+        
+        for(int i=0;i<n;i++)
+            li.add(new ArrayList<>());
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                //As self nodes are not considered
+                if(arr[i][j]==1 && i!=j)
+                li.get(i).add(j);
+            }
+        }
+        
+        
+        boolean[] visited = new boolean[n];
+        
+        for(int i=0;i<n;i++){
             if(visited[i]==false){
-                dfs(i,visited,arr);
+                dfs(i,li,visited);
                 count++;
             }
         }
         
         return count;
         
+        
+        
     }
-    
-    public void dfs(int src,boolean[] visited,int[][] arr){
-       
-         for(int j=0;j<arr.length;j++){
-            if(src==j)
-           continue;
-            if(arr[src][j]==1 && visited[j]==false){
-                visited[j] = true;
-                dfs(j,visited,arr);
-            }
-        }
-       
-    }
-    
 }
