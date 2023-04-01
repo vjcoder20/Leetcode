e09a09c725dfafd80c0baf304ef272de8211ab94
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-04-02 01:12:30 +0530

    Time: 7 ms (36.94%), Space: 43 MB (44.02%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean canFinish(int n, int[][] arr) {
+        
+      
+        
+        int[] indegree = new int[n];
+        
+        List<List<Integer>> li = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            li.add(new ArrayList<>());
+        }
+        
+        
+        for(int i=0;i<arr.length;i++){
+            li.get(arr[i][0]).add(arr[i][1]);
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int elem:li.get(i)){
+                indegree[elem]++;
+            }
+        }
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)
+            q.add(i);
+        }
+        
+        int count = 0;
+        
+        while(q.size()>0){
+            int rem = q.remove();
+            count++;
+            
+            for(int elem:li.get(rem)){
+                indegree[elem]--;
+                if(indegree[elem]==0)
+                q.add(elem);
+            }
+        }
+        
+       if(count==n)
+        return true;
+        
+        return false;
+    }
+}
