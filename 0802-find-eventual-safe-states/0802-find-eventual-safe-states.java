456780bc6b46e5121d9aac5d3148ec0efd112e87
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-04-03 01:38:48 +0530

    Time: 32 ms (22.89%), Space: 51.1 MB (38.19%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,62 +1,56 @@
-/*
-Basically i have used Detect cycle in a directed graph template to see:
+/*BFS Approach 
 
-->Any node who is part of cycle can't be part of safe node❌
-->Any node leads to cycle can't be part of safe node ❌
+Here you just have to reverse the graph and change outdegree to indegree respectively
+and apply topo sort
 
 */
+
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         
-        List<Integer> li = new ArrayList<>();
+        List<Integer> al = new ArrayList<>();
         int n = graph.length;
         
-        boolean[] visited = new boolean[n];
-        boolean[] path = new boolean[n];
-        boolean[] safe = new boolean[n];
+        List<List<Integer>> li = new ArrayList<>();
+        
+        for(int i=0;i<n;i++)
+            li.add(new ArrayList<>());
+        
+        
+        int[] indegree = new int[n];
         
         for(int i=0;i<n;i++){
-            if(visited[i]==false){
-                dfs(graph,i,visited,path,safe);
+            for(int elem:graph[i]){
+               li.get(elem).add(i);
+                indegree[i]++;
             }
         }
         
+        // System.out.println(li);
+        // System.out.println(Arrays.toString(indegree));
+    
+        Queue<Integer> q = new ArrayDeque<>();
         
         for(int i=0;i<n;i++){
-            if(safe[i]==true)
-                li.add(i);
+            if(indegree[i]==0)
+            q.add(i);
         }
         
-        return li;
-        
-    }
-    
-    
-public boolean dfs(int[][] graph,int src, boolean[] visited,boolean[] path,boolean[] safe){
-    
-    visited[src] = true;
-    path[src] = true;
-    
-    
-    for(int elem:graph[src]){
-        if(visited[elem]==false){
-            boolean c = dfs(graph,elem,visited,path,safe);
-            if(c==true)
-                return true;
+      
+        while(q.size()>0){
+            int rem = q.remove();
+            al.add(rem);
+            
+            for(int elem:li.get(rem)){
+                indegree[elem]--;
+                if(indegree[elem]==0)
+                q.add(elem);
+            }
         }
         
-        // if the node has been previously visited
-        // but it has to be visited on the same path
-        // then return true as these are not safe nodes so we don't want to
-        //do safe[src] = true for these nodes so return true from here only
+        Collections.sort(al);
+        return al;
         
-        else if(path[elem]==true)
-            return true;
-    }
-    
-    safe[src] = true;
-    path[src] = false;
-    return false;
 }
     
 }
