c9a13da93ec7b818c0d70584ec0c19f3547efbcf
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-04-11 01:25:57 +0530

    Time: 113 ms (46.82%), Space: 44.5 MB (61.82%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -11,8 +11,8 @@ class Solution {
     }
     
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+       
         Set<String> set = new HashSet<>();
-        Set<String> set2 = new HashSet<>();
         
         for(String w:wordList)
             set.add(w);
@@ -30,11 +30,11 @@ class Solution {
                     StringBuilder sb = new StringBuilder(str);
                     sb.setCharAt(i,(char)('a'+j));
                     String st = sb.toString();
-                    if(!set2.contains(st) && set.contains(st)){
+                    if(set.contains(st)){
                         if(st.equals(endWord))
                             return lvl+1;
                         q.add(new Pair(st,lvl+1));
-                        set2.add(st);
+                        set.remove(st);
                     }
                 }
             }
@@ -42,6 +42,5 @@ class Solution {
         }
         
         return 0;
-        
     }
 }
