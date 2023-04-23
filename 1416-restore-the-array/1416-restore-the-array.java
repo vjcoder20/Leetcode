87a6999d29f9eae50b61e10af0dbf88bb57c75bc
Vaibhav Jain <jain.vaibhav020@gmail.com>
2023-04-23 22:22:12 +0530

    Time: 341 ms (28.74%), Space: 64.2 MB (27.59%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    int mod = (int)1e9+7;
+    
+    public int dfs(int[] dp, int start, String s, int k) {
+     
+        if (dp[start] != 0)
+            return dp[start];
+
+
+        if (start == s.length())
+            return 1;
+
+       
+        if (s.charAt(start) == '0')
+            return 0;
+
+    
+        int count = 0;
+        for (int end = start; end < s.length(); ++end) {
+            String currNumber = s.substring(start, end + 1);
+            if (Long.parseLong(currNumber) > k)
+                break;
+            count = (count + dfs(dp, end + 1, s, k)) % mod;
+        }
+
+        dp[start] = count;
+        return count;
+    }
+    
+    public int numberOfArrays(String s, int k) {
+        int m = s.length();
+        int[] dp = new int[m + 1];
+        return dfs(dp, 0, s, k);
+    }
+}
